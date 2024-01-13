cbd9ce46fbf85ce71725b1f600c6bb4bf7ae37a0
Arsathirfan <67140008+Arsathirfan@users.noreply.github.com>
2024-01-13 12:24:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import cv2
 import base64
 import numpy as np
@@ -13,31 +13,37 @@ def index():
     return render_template('index.html')
 
 @app.route('/video_feed', methods=['POST'])
+@app.route('/video_feed', methods=['POST'])
+
+@app.route('/video_feed', methods=['POST'])
 def video_feed():
     try:
         image_data = request.json['image_data']
-        # Convert base64-encoded image data to NumPy array
-        img_bytes = base64.b64decode(image_data.split(',')[1])
+        img_bytes = base64.b64decode(image_data)
         img_array = np.frombuffer(img_bytes, dtype=np.uint8)
         frame = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-        # Convert the frame to grayscale for face detection
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Perform face detection
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
 
-        # Draw rectangles around the detected faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        if len(faces) > 0:
+            # Convert NumPy array elements to Python built-in types
+            faces_list = [face.tolist() for face in faces]
 
-        # Process the frame as needed
+            # If faces are detected, return the face detection coordinates
+            response_data = {'faces': faces_list}
+        else:
+            # If no faces are detected, return an empty list
+            response_data = {'faces': []}
 
-        # Convert the processed frame back to base64 for sending to the client (optional)
         _, encoded_frame = cv2.imencode('.jpg', frame)
         encoded_frame_data = base64.b64encode(encoded_frame.tobytes()).decode('utf-8')
 
-        return {'image_data': encoded_frame_data}
+        return jsonify(response_data)
+
     except Exception as e:
         print("Error processing frame:", str(e))
         return '', 204  # No content response
+
+
+
